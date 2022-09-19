4f2c6758f93069d7d628851a4d31da3a727193fc
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-09-20 00:00:26 +0300

    Landing page placeholder

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='h-screen border-2 w-full flex justify-center items-center'>
+      <p className='text-4xl text-red-900'>PILAR MIRALLES</p>
+    </div>
+  )
+}
+
+export default Landing
