e60697f8c93157808add31c12d9c9033a387c269
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-09-20 22:59:02 +0300

    Landing page draft

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -2,9 +2,33 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div className='h-screen border-2 w-full flex justify-center items-center'>
-      <p className='text-4xl text-red-900'>PILAR MIRALLES</p>
-    </div>
+    <section className='h-[calc(100vh-theme(spacing.14))]  w-full flex flex-col items-center pt-20 px-2 xl:flex-row xl:justify-center'>
+      <div className='flex items-center justify-center mb-6 xl:mr-6'>
+        <img src='https://via.placeholder.com/400' alt='' />
+      </div>
+      <article>
+        <header className='text-4xl text-red-900 mb-2'>PILAR MIRALLES</header>
+        <p className='max-w-xl text-justify'>
+          Nisi excepteur esse laborum aute nostrud et duis proident. Ea ea ut
+          adipisicing nulla nulla non quis. Non id in irure quis minim nisi
+          veniam sit Lorem excepteur ex pariatur. Nostrud non qui qui consequat
+          labore. Anim dolore ea irure aute incididunt ea amet sunt adipisicing
+          esse deserunt aliqua. Velit quis incididunt eu tempor dolor veniam
+          excepteur do proident nulla nisi laboris. Pariatur amet dolore culpa
+          consequat. Esse amet fugiat laborum ex fugiat mollit. Incididunt irure
+          mollit dolor dolor eiusmod do Lorem exercitation magna labore. Dolore
+          culpa aute dolore commodo mollit dolore adipisicing laborum non
+          laboris veniam voluptate ea. Dolore nisi et amet aute esse dolore ex
+          dolor consectetur duis cupidatat. Et non nulla quis reprehenderit duis
+          consequat enim pariatur. Sit veniam laboris Lorem voluptate dolor
+          exercitation tempor laborum proident. Duis reprehenderit occaecat duis
+          ad. Ipsum Lorem reprehenderit cillum nisi eiusmod sint officia
+          proident sint nostrud voluptate. Cillum consectetur irure aute enim
+          cillum exercitation amet duis ad. Sunt dolor sunt laboris sit et ea
+          laborum velit deserunt proident.
+        </p>
+      </article>
+    </section>
   )
 }
 
