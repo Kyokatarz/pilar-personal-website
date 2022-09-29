0cccc2e1d88bf1358c6a29292035b9260cb34604
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-09-30 00:54:41 +0300

    About section

--- src/pages/Landing/components/About/About.tsx
+++ src/pages/Landing/components/About/About.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Article from '../../../../components/Article'
+import Paragraph from '../../../../components/Paragraph'
+import Section from '../../../../components/Section'
+import SeeMore from '../../../../components/SeeMore'
 
 const About = () => {
-  return <div></div>
+  return (
+    <Section>
+      <Article>
+        <h1 className='text-6xl font-bold text-left mb-4'>Hi,</h1>
+        <Paragraph>
+          My name is Pilar Miralles and I was born in Almería (Spain) in 1997. I
+          am a composer and sound artist currently based in Helsinki (Finland).
+          I speak Spanish, English and a bit of French. You can find my artist
+          statement in the home page.
+        </Paragraph>
+        <SeeMore>See more about me {'>>'}</SeeMore>
+      </Article>
+    </Section>
+  )
 }
 
 export default About
