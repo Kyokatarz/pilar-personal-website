e9a3d304cc95504d864957a75663e87080780c7f
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-10-05 23:45:53 +0300

    Sending data from client

--- src/pages/Landing/sections/Contact/Contact.tsx
+++ src/pages/Landing/sections/Contact/Contact.tsx
@@ -2,11 +2,32 @@ import React from 'react'
 import { FaFacebook } from 'react-icons/fa'
 import { ImSoundcloud2 } from 'react-icons/im'
 import { IconContext } from 'react-icons'
+import axios from 'axios'
 
 import Article from '../../../../components/Article'
 import Section from '../../../../components/Section'
 
 const Contact = () => {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [message, setMessage] = React.useState('')
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log('submitting form')
+
+    try {
+      const res = await axios.post('http://localhost:8080/api/v1/sendMail', {
+        name,
+        email,
+        message,
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <Section>
       <Article>
@@ -53,22 +74,28 @@ const Contact = () => {
             </IconContext.Provider>
           </a>
         </div>
-        <form className='flex flex-col items-start'>
+        <form className='flex flex-col items-start' onSubmit={handleSubmit}>
           <input
             type='text'
             placeholder='Name...'
             className='mb-2 border-black border-b-2 w-full focus:outline-none text-lg pb-2'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             type='email'
             placeholder='Email...'
             className='mb-2 border-black border-b-2 w-full focus:outline-none text-lg pb-2'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <textarea
             placeholder='Message...'
             className='mb-2 border-black border-b-2 w-full focus:outline-none text-lg pb-2'
             style={{ resize: 'none' }}
             rows={5}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
           <button
             className='mt-2 py-2 px-4 border-black border text-white bg-black rounded-sm'
