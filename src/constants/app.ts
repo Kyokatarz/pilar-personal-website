353804f5c8b5b397a7ae4fafcb01234a1174ff96
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-10-06 00:59:37 +0300

    Fix constants front end

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,4 +1,4 @@
 export const serverEndpont =
   process.env.NODE_ENV === 'production'
-    ? 'https://pilar-website-server.fly.dev/'
+    ? 'https://pilar-website-server.fly.dev'
     : 'http://localhost:8080'
