574b0bdaeff707902a3565515ddd6551e89ffc72
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-10-08 14:02:16 +0300

    init react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
+import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <main className='pt-14'>
-        <Landing />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
       </main>
     </div>
   )
