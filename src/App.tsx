c8de02c33512015268e2a25c32d9bf13b34babb0
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-10-08 16:09:18 +0300

    Media page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
 import './App.css'
+import Media from './pages/Media'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className='pt-14'>
         <Routes>
           <Route path='/' element={<Landing />} />
+          <Route path='/media' element={<Media />} />
         </Routes>
       </main>
     </div>
