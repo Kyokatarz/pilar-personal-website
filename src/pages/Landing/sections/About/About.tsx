861c341cd96122c355cc8dc03e9c733c51763c51
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-10-10 15:24:41 +0300

    Minor adjustments

--- src/pages/Landing/sections/About/About.tsx
+++ src/pages/Landing/sections/About/About.tsx
@@ -12,8 +12,11 @@ const About = () => {
         <Paragraph>
           My name is Pilar Miralles and I was born in Almería (Spain) in 1997. I
           am a composer and sound artist currently based in Helsinki (Finland).
-          I speak Spanish, English and a bit of French. You can find my artist
-          statement in the home page.
+          I speak Spanish, English and a bit of French. After having graduated
+          from the University of the Arts Helsinki, I am focused on expanding my
+          personal artistic practice and knowledge, seeking intermedia
+          collaborations and embracing a constant attitude of opening,
+          perceiving and questioning.
         </Paragraph>
         <SeeMore to='/about-me'>See more about me {'>>'}</SeeMore>
       </Article>
