7bad00a3ac1cc7f9e3da5a0667b0dfd524811aca
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-10-17 00:05:17 +0300

    Add thesis

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,11 +1,18 @@
+import clsx from 'clsx'
 import React from 'react'
 
 type Props = {
   children: React.ReactNode
+  level?: number
+  listStyle?: 'list-disc' | 'list-square'
 }
 
-const ListItem = ({ children }: Props) => {
-  return <li className='list-disc ml-4'>{children}</li>
+const ListItem = ({ children, level = 1, listStyle = 'list-disc' }: Props) => {
+  const levelIndent = `ml-${level * 4}`
+
+  const className = clsx(listStyle, levelIndent)
+
+  return <li className={className}>{children}</li>
 }
 
 export default ListItem
