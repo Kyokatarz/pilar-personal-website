8197bcf08d34102e0597caa584b6999587101c4a
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2022-10-27 01:26:54 +0300

    Page design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import MobileNavLinks from './components/MobileNavLinks'
 
 const Navbar = () => {
   return (
-    <nav className='w-full h-14 fixed flex flex-row items-center justify-between bg-white drop-shadow-md'>
+    <nav className='w-full h-14 fixed flex flex-row items-center justify-between bg-white drop-shadow-md border-b-2 border-black'>
       <a href='/#landing-introduction'>
         <img src={icon} alt='To home page icon' className='ml-4' />
       </a>
